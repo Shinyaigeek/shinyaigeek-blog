0bc3f3153e62b9917c550b70afa76b2401d082f0
Shinyaigeek <PhilisPaxil@gmail.com>
2019-12-27 15:30:51 +0900

    [fix]configuration

--- static.config.js
+++ static.config.js
@@ -6,6 +6,16 @@ import path from 'path'
 
 export default {
   entry: path.join(__dirname, 'src', 'index.tsx'),
+  paths: {
+    root: process.cwd(), // The root of your project. Don't change this unless you know what you're doing.
+    src: 'src', // The source directory. Must include an index.js entry file.
+    temp: 'tmp', // Temp output directory for build files not to be published.
+    dist: 'public', // The production output directory.
+    devDist: 'tmp/dev-server', // The development scratch directory.
+    public: 'metas', // The public directory (files copied to dist during build)
+    assets: 'public', // The output directory for bundled JS and CSS
+    buildArtifacts: 'artifacts', // The output directory for generated (internal) resources
+  },
   getRoutes: async () => {
     const { data: posts } /* :{ data: Post[] } */ = await axios.get(
       'https://jsonplaceholder.typicode.com/posts'
