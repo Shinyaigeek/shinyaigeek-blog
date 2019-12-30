575c1d2413dd3aeb02f3acdb8a34a13ddb6d7859
Shinyaigeek <PhilisPaxil@gmail.com>
2019-12-30 23:01:57 +0900

    [add]profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React, { useState } from "react";
-import { Root, Routes, addPrefetchExcludes } from "react-static";
+import { Root, Routes } from "react-static";
 import { Router } from "@reach/router";
 import "./app.css";
 import Header from "components/Header";
 import Footer from "components/Footer";
-
-// Any routes that start with 'dynamic' will be treated as non-static routes
-addPrefetchExcludes(["dynamic"]);
+import Profile from "./pages/profile";
 
 function App() {
   const [showContactModal, setShowContactModal] = useState(false);
   const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
-  
+
   return (
     <Root>
-      <Header showContactModal={showContactModal} setShowContactModal={() => setShowContactModal(!showContactModal)} showHamburgerMenu={showHamburgerMenu} setShowHamburgerMenu={() => setShowHamburgerMenu(!showHamburgerMenu)} />
-      <div style={{
-        position: "relative",
-        top: "72px"
-      }}>
-        <React.Suspense fallback={<em>Loading...</em>}>
+      <Header
+        showContactModal={showContactModal}
+        setShowContactModal={() => setShowContactModal(!showContactModal)}
+        showHamburgerMenu={showHamburgerMenu}
+        setShowHamburgerMenu={() => setShowHamburgerMenu(!showHamburgerMenu)}
+      />
+      <div
+        style={{
+          position: "relative",
+          top: "72px"
+        }}
+      >
+        <React.Suspense fallback={() => <div>loading</div>}>
           <Router>
+            <Routes
+              path="profile"
+              render={() => {
+                return (
+                  <Profile
+                    setShowContactModal={() =>
+                      setShowContactModal(!showContactModal)
+                    }
+                  />
+                );
+              }}
+            />
             <Routes path="*" />
           </Router>
         </React.Suspense>
